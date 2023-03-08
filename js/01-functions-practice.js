f3b39b1d55ddc80b321ffd149a34f9a14ba5805f
Kevin-Kuzniczci <60890825+Kevin-Kuzniczci@users.noreply.github.com>
2023-03-08 12:16:34 -0800

    First part done, will complete later today

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,9 +1,53 @@
 //STEP 1
 
+// let originalNumber = prompt('Please select a number.')
+
+// function halfNumber() {
+//     let newNumber = originalNumber / 2
+
+//     console.log("Half of " + originalNumber + " is " + newNumber)
+// }
+
+// halfNumber()
+
 //STEP 2
+
+// let originalNumber = prompt('Please select a number.')
+
+// function squareNumber() {
+//     let newNumber = originalNumber * originalNumber
+
+//     console.log("The result of squaring " + originalNumber + " is " + newNumber)
+// }
+
+// squareNumber()
 
 //STEP 3
 
+// let originalNumber = prompt('Please select a number.')
+
+// let originalNumber2 = prompt('Please select a second number.')
+
+// function percentOf() {
+//     let answer = ((originalNumber2 - originalNumber) / originalNumber) * 100;
+
+//     console.log(originalNumber + " is " + answer + "% of " + originalNumber2)
+// }
+
+// percentOf();
+
 //STEP 4
+
+// let originalNumber = prompt('Please select a number.')
+
+// let originalNumber2 = prompt('Please select a second number.')
+
+// function findModulus() {
+//     let theModulus = originalNumber % originalNumber2
+
+//     console.log(theModulus + " is the modulus of " + originalNumber + " and " + originalNumber2)
+// }
+
+// findModulus()
 
 //STEP 5
