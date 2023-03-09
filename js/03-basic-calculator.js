761cd30cc4bccf2d24ef75d992fbf8a8c6594af4
Kevin-Kuzniczci <60890825+Kevin-Kuzniczci@users.noreply.github.com>
2023-03-08 19:16:45 -0800

    Finished the calculator

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,8 +1,38 @@
 // IMPORT THE MODULE
+import { add } from "./modules/calculator.js"
+import { subtraction } from "./modules/calculator.js"
+import { multiplication } from "./modules/calculator.js"
+import { division } from "./modules/calculator.js"
 
 // COLLECT FIRST NUMBER FROM USER
+// let firstNumber = prompt('Please select your first number.')
+
 // COLLECT SECOND NUMBER FROM USER
+// let secondNumber = prompt('Please select your second number.')
+
 // COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
+let chosenOperation = prompt('Please choose between +, =, *, and /.')
 
 // CHECK TO SEE WHAT OPERATION THEY'RE USING
 // CALL THE APPROPRIATE FUNCTION
+
+function calculate() {
+    switch (chosenOperation) {
+        case "+":
+            alert('Your result is ' + add())
+            break;
+        case "-":
+            alert('Your result is ' + subtraction())
+            break;
+        case "*":
+            alert('Your result is ' + multiplication())
+            break;
+        case "/":
+            alert('Your result is ' + division())
+            break;
+        default:
+            alert('Error: cannot calculate. Please utilize the exact inputs. Please refresh the page.')
+    }
+}
+
+calculate()
--- js/modules/calculator.js
+++ js/modules/calculator.js
@@ -1,13 +1,47 @@
 // ADD A PRIVATE FUNCTION CALLED CALCULATE
+let firstNumber = prompt('Please select your first number.')
 
+let secondNumber = prompt('Please select your second number.')
+// function calculate() {
+//     switch (chosenOperation) {
+//         case "+":
+//             alert('Your result is ' + add(firstNumber, secondNumber))
+//             break;
+//         case "-":
+//             alert('Your result is ' + subtraction(firstNumber, secondNumber))
+//             break;
+//         case "*":
+//             alert('Your result is ' + multiplication(firstNumber, secondNumber))
+//             break;
+//         case "/":
+//             alert('Your result is ' + division(firstNumber, secondNumber))
+//             break;
+//         default:
+//             alert('Error: cannot calculate. Please utilize the exact inputs.')
+//     }
+// }
 // ADD FOUR PUBLIC FUNCTIONS BELOW
 //////////////////////////////////
 // ADD FUNCTION
-
+export function add() {
+    return Number(firstNumber) + Number(secondNumber)
+}
 // SUBTRACT FUNCTION
-
+export function subtraction() {
+    return Number(firstNumber) - Number(secondNumber)
+}
 // MULTIPLY FUNCTION
-
+export function multiplication()  {
+    return Number(firstNumber) * Number(secondNumber)
+}
 // DIVIDE FUNCTION
-
+export function division() {
+    return Number(firstNumber) / Number(secondNumber)
+}
 // EXPORT THE FOUR PUBLIC FUNCTIONS
+
+// export function add();
+// export function subtraction();
+// export function multiplication();
+// export function division();
+// export function calculate();
